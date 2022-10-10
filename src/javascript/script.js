ceefe4d9f78251d66fd17fd32cabcb1f536adc80
Jonathan W <jwilliams12117@gmail.com>
2022-10-09 19:24:05 -0700

    Changed inputs to be part of a form, added script that converted from JPY to USD or USD to JPY and formatted to page

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,4 +1,5 @@
 // element definitions
+const exchangeFormEl = document.querySelector("#exchange-form");
 const usdTextEl = document.querySelector("#usd-text");
 const jpyTextEl = document.querySelector("#jpy-text");
 const calculateButtonEl = document.querySelector("#calculate");
@@ -7,28 +8,64 @@ const displayEl = document.querySelector("#display");
 // Switch to tell what to calculate
 let toUSD = true;
 
+// Current exchange rate (expressed in USD:JPY. Use reciprocal for JPY:USD.)
+let exchangeRate = 2;
+
 // Script to pull exchange rate data and store in localStorage
+// If we cannot pull the data from online, display the last known exchange rate from localStorage
 
 // Script to convert unix time to readable format
 
-// Script to calculate the exchange rate
-// If we cannot pull the data from online, display the last known exchange rate from localStorage
-
 // Script to calculate either the JPY:USD or USD:JPY depending on which text box the user selected,
 // and display it to the result div
+const calculateCost = () => {
+    console.log("Calculating cost of item...");
+    // Check whether toUSD is false
+    if (toUSD === false) {
+        let formatter = new Intl.NumberFormat("en-us", {
+            style: "currency",
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+            currency: "JPY"
+        });
+        let result = parseFloat(usdTextEl.value.trim());
+        result = formatter.format(result * exchangeRate);
+        displayEl.textContent = result;
+    }
+    else if (toUSD === true) {
+        let formatter = new Intl.NumberFormat("en-us", {
+            style: "currency",
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+            currency: "USD"
+        });
+        let result = parseFloat(jpyTextEl.value.trim());
+        result = formatter.format(result * (1 / exchangeRate));
+        displayEl.textContent = result;
+        
+    }
+    // Clear the inputs
+}
 
 
 // Event listeners...
 
 // Set calculation mode on user click into JPY or USD box
 usdTextEl.addEventListener("click", () => {
-    toUSD = true;
-    console.log("Converting from JPY to USD.")
-});
-
-jpyTextEl.addEventListener("click", () => {
     toUSD = false;
     console.log("Converting from USD to JPY.")
 });
 
+jpyTextEl.addEventListener("click", () => {
+    toUSD = true;
+    console.log("Converting from JPY to USD.")
+});
+
 // Run calculation on click of calculate button
+
+exchangeFormEl.addEventListener("submit", function(event) {
+    event.preventDefault();
+    calculateCost();
+    usdTextEl.value = "";
+    jpyTextEl.value = "";
+});
